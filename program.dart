01c405b2841ae6f8608837acaa409a9440a68025
JojoSkyler <159585712+JojoSkyler@users.noreply.github.com>
2024-02-29 14:47:20 +0300

    Add files via upload

---
+++ program.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Integer data type (int)
+  int age = 18;
+
+  // Double data type (double)
+  double height = 5.9;
+
+  // String data type (String)
+  String name = "Joan Skyler";
+
+  // List data type (List)
+  List<String> hobbies = ["Coding", "Dancing", "Cooking"];
+
+  // Map data type (Map)
+  Map<String, dynamic> person = {
+    'name': 'Felix Rich',
+    'age': 18,
+    'height': 6.0,
+    'hobbies': ['Coding', 'Walking']
+  };
+
+  // Printing the values
+  print("Age: $age");
+  print("Height: $height");
+  print("Name: $name");
+  print("Hobbies: $hobbies");
+  print("Person: $person");
+}
